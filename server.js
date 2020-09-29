581879825f0487e810f186a381c6acec9d0a638c
manik8 <maniksharma2014.ms@gmail.com>
2020-09-29 21:11:42 +0530

    some changes

--- server.js
+++ server.js
@@ -1,56 +1,56 @@
 var express = require("express");
 var path = require("path");
+var favicon = require("serve-favicon");
+var logger = require("morgan");
+var cookieParser = require("cookie-parser");
 var bodyParser = require("body-parser");
 var passport = require("passport");
 var Strategy = require("passport-twitter").Strategy;
 var session = require("express-session");
-var http = require("http");
-http
-  .createServer(function (req, res) {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Hello World\n");
-  })
-  .listen(3000, "127.0.0.1");
-console.log("Server running at http://127.0.0.1:3000/");
-passport.use(
-  new Strategy(
-    {
-      consumerKey: "JAABlOt9wzw9dyr8SASkPjRrj",
-      consumerSecret: "ki0m1aFKtdYisdalDQUOHnfOS0EI5XC1Iez1xbhx0Htox2NwrI",
-      callbackURL: "http://localhost:3000",
-    },
-    function (token, tokenSecret, profile, callback) {
-      return callback(null, profile);
-    }
-  )
-);
 
 passport.serializeUser(function (user, callback) {
   callback(null, user);
 });
 
-passport.deserializeUser(function (user, callback) {
-  callback(null, user);
+passport.deserializeUser(function (obj, callback) {
+  callback(null, obj);
 });
 
 var app = express();
 
-app.set("views", path.join(__dirname), "views");
-app.set("view engine", "manik");
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "html");
 
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(
-  session({
-    secret: "whatever",
-    resave: true,
-    saveUninitialized: true,
-  })
-);
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+app.use(session({ secret: "whatever", resave: true, saveUninitialized: true }));
+
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.get("/", (req, res) => {
-  res.send("hello");
+//
+
+app.get("/", function (req, res) {
+  res.render("index", { user: req.user });
 });
+
+app.get("/twitter/login", passport.authenticate("twitter"));
+
+app.get(
+  "/twitter/return",
+  passport.authenticate("twitter", {
+    failureRedirect: "/",
+  }),
+  function (req, res) {
+    res.redirect("/");
+  }
+);
+
+app.listen(3000, () => console.log(`Example app listening on port ${3000}!`));
 module.exports = app;
